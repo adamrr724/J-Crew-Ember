d1f9e150dfb39591ea345abde9a2e21b8b4f3b8a
adam ross russell and brooke hurford <adamrr724@yahoo.com>
2016-04-07 15:34:36 -0700

    Styled product detail page, and changed style after item is added to the cart

--- app/components/product-detail.js
+++ app/components/product-detail.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  itemAdded: false,
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
+      this.set('itemAdded', true);
     }
   }
 });
