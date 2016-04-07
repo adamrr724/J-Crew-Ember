48852b8b149c99e8b55104c9d7b3f663cec4a957
adam ross russell and brooke hurford <adamrr724@yahoo.com>
2016-04-07 16:33:54 -0700

    Style update product form, and add shopping cart counter to the nav

--- app/helpers/star-rating.js
+++ app/helpers/star-rating.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export function starRating(params) {
   var reviewRating = params[0].get('rating');
 
-  if(reviewRating = "5 stars") {
+  if(reviewRating == "5 stars") {
     return Ember.String.htmlSafe('<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>');
-  } else if(reviewRating = "4 stars") {
+  } else if(reviewRating == "4 stars") {
     return Ember.String.htmlSafe('<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>');
-  } else if(reviewRating = "3 stars") {
+  } else if(reviewRating == "3 stars") {
     return Ember.String.htmlSafe('<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>');
-  } else if(reviewRating = "2 stars") {
+  } else if(reviewRating == "2 stars") {
     return Ember.String.htmlSafe('<i class="fa fa-star"></i><i class="fa fa-star"></i>');
   } else {
     return Ember.String.htmlSafe('<i class="fa fa-star"></i>');
